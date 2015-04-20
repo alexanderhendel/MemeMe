611a1972a9b8b35b4826f9d8f1450230cd7b9863
hiro42 <hiro@MacBook-Air.local>
2015-04-20 17:34:33 +0200

    Properly organize code with MARK: and implement ShareSheet

--- MemeMe/EditMemeViewController.swift
+++ MemeMe/EditMemeViewController.swift
@@ -13,21 +13,31 @@ class EditMemeViewController: UIViewController,
                               UINavigationControllerDelegate
 {
 
-    // IBOutlet
-    @IBOutlet weak var memeImage: UIImageView!
+    // MARK: - IBOutlet
+    @IBOutlet weak var memeImageView: UIImageView!
     @IBOutlet weak var memeTopLabel: UITextField!
     @IBOutlet weak var memeBottomLabel: UITextField!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var toolbar: UIToolbar!
     
-    // Delegates
+    // MARK: - Delegates
     let MemeTextFieldDelegate = memeTextFieldDelegate()
     
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // setup delegates
         memeTopLabel.delegate = MemeTextFieldDelegate
         memeBottomLabel.delegate = MemeTextFieldDelegate
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -53,6 +63,11 @@ class EditMemeViewController: UIViewController,
         
         memeBottomLabel.textAlignment = NSTextAlignment.Center
         memeBottomLabel.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        
+        // disable share Buttom only when imageView is empty
+        if self.memeImageView.image == nil {
+            shareButton.enabled = false
+        }
     }
 
     override func viewWillDisappear(animated: Bool) {
@@ -61,6 +76,7 @@ class EditMemeViewController: UIViewController,
         self.unsubscribeFromKeyboardNotifications()
     }
     
+    // MARK: - Keyboard Notifications
     func keyboardWillShow(notification: NSNotification) {
         
         //TODO: move keyboard
@@ -98,7 +114,8 @@ class EditMemeViewController: UIViewController,
         NSNotificationCenter.defaultCenter().removeObserver(self,
             name: UIKeyboardWillHideNotification, object: nil)
     }
-    
+
+    // MARK: - Custom Functions
     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
         
         let userInfo = notification.userInfo
@@ -108,25 +125,72 @@ class EditMemeViewController: UIViewController,
         return keyboardSize.CGRectValue().height
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func save() {
+    
+        // setup meme object
+        var myMeme = meme()
+        
+        myMeme.topText = memeTopLabel.text
+        myMeme.bottomText = memeBottomLabel.text
+        myMeme.image = memeImageView.image
+            
+        myMeme.memeImage = self.generateMemedImage()
+    }
+
+    @IBAction func shareMeme(sender: UIBarButtonItem) {
+        
+        // share your fancy meme :)
+        let activityItem = generateMemedImage()
+        let activityViewController = UIActivityViewController(activityItems: [activityItem],
+            applicationActivities: nil)
+        
+        self.presentViewController(activityViewController, animated: true, completion: nil)
     }
     
-    @IBAction func cancelMemeEdit(sender: UIBarButtonItem) {
+    func generateMemedImage() -> UIImage {
         
-        self.dismissViewControllerAnimated(true, completion: nil)
+        // Hide toolbar and navbar
+        self.navigationController?.navigationBar.hidden = true
+        self.toolbar.hidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        
+        self.view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        
+        let memedImage : UIImage =
+            UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        
+        // Show toolbar and navbar
+        self.navigationController?.navigationBar.hidden = false
+        self.toolbar.hidden = false
+        
+        return memedImage
     }
-
-    // show image picker with different sources (Camera or Photo Library)
+    
     func showImagePickerOfType(pickerType: UIImagePickerControllerSourceType) {
-
+        
+        // show image picker with different sources (Camera or Photo Library)
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = pickerType
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: - Image Picker Protocol
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            memeImageView.image = image
+            shareButton.enabled = true
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    // MARK: - IBActions
     @IBAction func pickImageFromCamera(sender: UIBarButtonItem) {
         
         self.showImagePickerOfType(UIImagePickerControllerSourceType.Camera)
@@ -137,11 +201,9 @@ class EditMemeViewController: UIViewController,
         self.showImagePickerOfType(UIImagePickerControllerSourceType.PhotoLibrary)
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            memeImage.image = image
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }
+    @IBAction func cancelMemeEdit(sender: UIBarButtonItem) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
--- MemeMe/MemeMeModel.swift
+++ MemeMe/MemeMeModel.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class meme: NSObject {
+
+    var topText: String?
+    var bottomText: String?
+    var image: UIImage?
+    var memeImage: UIImage?
+    /*
+    convenience init(image: UIImage!, topText: String!, bottomText: String!) {
+        
+        self.init()
+        
+        if topText != nil {
+            self.topText = topText!
+        }
+        
+        if bottomText != nil {
+            self.bottomText = bottomText!
+        }
+        
+        if image != nil {
+            self.image = image
+        }
+    }
+    */
+}
